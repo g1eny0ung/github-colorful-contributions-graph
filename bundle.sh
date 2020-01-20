31d0d84cda42c08d1b121cf666c0dd8efd28852b
Yue Yang <g1enyy0ung@gmail.com>
2020-01-20 11:32:56 +0800

    Update bundle.sh

--- bundle.sh
+++ bundle.sh
@@ -2,19 +2,17 @@ echo "Move images --- "
 
 if [ -d "bundle/images" ]; then
   rm -r bundle/images
-  cp -r resources/public/images bundle
-else
-  cp -r resources/public/images bundle
 fi
+
+cp -r resources/public/images bundle
 
 echo "Move css files --- "
 
 if [ -d "bundle/css" ]; then
   rm -r bundle/css
-  cp -r resources/public/css bundle
-else
-  cp -r resources/public/css bundle
 fi
+
+cp -r resources/public/css bundle
 
 echo "Bundle js --- "
 
@@ -24,13 +22,19 @@ echo "Move js files --- "
 
 if [ -d "bundle/js" ]; then
   rm -r bundle/js
-  cp -r resources/public/js bundle
-else
-  cp -r resources/public/js bundle
 fi
+
+cp -r resources/public/js bundle
 
 cp resources/public/cljs-out/min-main.js bundle/js/github_colorful_contributions.js
 
 cp resources/public/manifest.json bundle/
 
-zip -r g-c-c.zip bundle
+rm -r bundle/js/closure.dev.js
+rm -r bundle/js/goog.dev.js
+
+if [ -f "g-c-c.zip" ]; then
+  rm g-c-c.zip 
+fi
+
+zip -r g-c-c bundle
