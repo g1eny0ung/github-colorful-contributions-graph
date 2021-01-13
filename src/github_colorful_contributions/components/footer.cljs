b7a201f2a5e256d5f01c039c355bee662bd7f3e5
Yue Yang <g1enyy0ung@gmail.com>
2021-01-13 18:50:27 +0800

    style: optimized

--- src/github_colorful_contributions/components/footer.cljs
+++ src/github_colorful_contributions/components/footer.cljs
@@ -5,5 +5,7 @@
    [:h3.subtitle.is-7.has-text-right "Made with "
     [:span {:style {:color "red"}} "❤"]
     " and fun by "
-    [:span {:style {:color "seagreen"}} "g1eny0ung"]
+    [:span {:style {:color "seagreen" :cursor "pointer"}
+            :on-click #(.create (.. js/chrome -tabs)
+                                (clj->js {:url "https://github.com/g1eny0ung"}))} "g1eny0ung"]
     "."]])
