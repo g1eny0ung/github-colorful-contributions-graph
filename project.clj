57a5ad8c09b814731777a12b900caf26ab567e9a
Yue Yang <g1enyy0ung@gmail.com>
2022-01-24 22:55:33 +0800

    Release 4.0.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject github-colorful-contributions-graph "3.0.5"
+(defproject github-colorful-contributions-graph "4.0.1"
   :description "A Chrome extension for customizing the colors of the GitHub contributions graph."
   :url "http://github.com/g1eny0ung/github-colorful-contributions-graph"
   :license {:name "Eclipse Public License"
