a6e713965419ac8e05a42ff1dd46c36d68e31759
Yue Yang <g1enyy0ung@gmail.com>
2026-04-17 19:12:25 +0800

    chore: update wxt

--- wxt.config.firefox.ts
+++ wxt.config.firefox.ts
@@ -14,8 +14,6 @@ export default defineConfig({
     browser_specific_settings: {
       gecko: {
         id: 'github-colorful-contributions@app.g1en.dev',
-        // https://github.com/wxt-dev/wxt/issues/1975
-        // @ts-ignore
         data_collection_permissions: {
           required: ['none'],
         },
